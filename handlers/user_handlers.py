225371bda3e9ba21207f5d44ea98375b59a781ef
5ergey <akifyev.sergey@gmail.com>
2025-02-22 09:17:51 +0300

    Добавлена отправка картинок

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,14 +1,16 @@
 from aiogram import Router, F
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import default_state, State
-from aiogram.types import Message, ReplyKeyboardRemove
+from aiogram.types import Message, ReplyKeyboardRemove, FSInputFile
 from aiogram.filters import Command, CommandStart, StateFilter
 from lexicon.lexicon_ru import LEXICON_RU, PROMPTS_RU
 from keyboards import kb_start, kb_random
 from states import Chat
 from gpt import gpt_text
+from os import path
 
 user_router = Router()
+images_dir = path.join(path.dirname(path.abspath(__file__)), 'images')
 
 @user_router.message(F.text == 'Закончить')
 @user_router.message(CommandStart())
@@ -35,6 +37,8 @@ async def process_help_command(message: Message, state: FSMContext):
 @user_router.message(Command('gpt'))
 async def process_gpt_command(message: Message, state: FSMContext):
     await state.set_state(Chat.chat)
+    photo_file = FSInputFile(path=path.join(images_dir, 'chat-gpt.jpg'))
+    await message.answer_photo(photo=photo_file)
     await message.answer(
         text=LEXICON_RU['/gpt'].replace('name', message.from_user.full_name),
         reply_markup=ReplyKeyboardRemove()
@@ -44,6 +48,8 @@ async def process_gpt_command(message: Message, state: FSMContext):
 @user_router.message(Command('random'))
 async def process_random_command(message: Message, state: FSMContext):
     await state.set_state(Chat.random)
+    photo_file = FSInputFile(path=path.join(images_dir, 'fact.jpg'))
+    await message.answer_photo(photo=photo_file)
     await message.answer(
         text=LEXICON_RU['/random'].replace('name', message.from_user.full_name),
     )
